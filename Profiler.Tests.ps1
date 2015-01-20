9009d2fd99e89e75f592ff77680babed12cf4cdd
Dave Wyatt <dlwyatt115@gmail.com>
2015-01-20 08:11:12 -0500

    Tests update
    Acceptance test for hit counts with a loop.

--- Profiler.Tests.ps1
+++ Profiler.Tests.ps1
@@ -11,10 +11,10 @@ Describe 'Get-ScriptPerfData' {
             Write-Verbose World
             Start-Sleep -Milliseconds 100
 '@
-        
+
         $report = Get-ScriptPerfData -Path TestDrive:\test.ps1 -ScriptBlock { TestDrive:\test.ps1 } |
                   Sort-Object Line
-        
+
         It 'Assigns timing values to the correct lines' {
             $report[0].TotalMilliseconds | Should BeLessThan 10
             $report[1].TotalMilliseconds | Should BeGreaterThan 150
@@ -22,6 +22,33 @@ Describe 'Get-ScriptPerfData' {
             $report[2].TotalMilliseconds | Should BeLessThan 10
             $report[3].TotalMilliseconds | Should BeGreaterThan 50
             $report[3].TotalMilliseconds | Should BeLessThan 150
+        }
+    }
+
+    Context 'Hit counts' {
+        Set-Content -LiteralPath TestDrive:\test.ps1 -Value @'
+            Write-Verbose Hello
+            foreach ($number in 1..5)
+            {
+                Write-Verbose World
+            }
+            Start-Sleep -Milliseconds 100
+'@
+
+        $report = Get-ScriptPerfData -Path TestDrive:\test.ps1 -ScriptBlock { TestDrive:\test.ps1 }
+
+        $hash = @{}
+
+        foreach ($item in $report)
+        {
+            $hash[[int]($item.Line)] = $item
+        }
+        
+        It 'Assigns the correct hit counts' {
+            $hash[1].HitCount | Should Be 1
+            $hash[2].HitCount | Should Be 1
+            $hash[4].HitCount | Should Be 5
+            $hash[6].HitCount | Should Be 1
         }
     }
 }
